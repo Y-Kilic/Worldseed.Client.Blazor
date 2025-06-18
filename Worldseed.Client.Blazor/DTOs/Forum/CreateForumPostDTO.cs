83cfc177780cea987ab51ccd3f80f73034e38341
Y-Kilic <CyanGithub@protonmail.com>
2025-06-18 15:02:51 +0200

    Derive owner from auth token

--- Worldseed.Client.Blazor/DTOs/Forum/CreateForumPostDTO.cs
+++ Worldseed.Client.Blazor/DTOs/Forum/CreateForumPostDTO.cs
@@ -3,7 +3,6 @@ namespace Worldseed.Client.Blazor.DTOs.Forum
     public class CreateForumPostDTO
     {
         public int ForumCategoryThreadId { get; set; }
-        public int OwnerId { get; set; }
         public string Content { get; set; }
     }
 }
--- Worldseed.Client.Blazor/DTOs/Forum/CreateForumThreadDTO.cs
+++ Worldseed.Client.Blazor/DTOs/Forum/CreateForumThreadDTO.cs
@@ -3,7 +3,6 @@ namespace Worldseed.Client.Blazor.DTOs.Forum
     public class CreateForumThreadDTO
     {
         public int ForumCategoryId { get; set; }
-        public int OwnerId { get; set; }
         public string Title { get; set; }
     }
 }
